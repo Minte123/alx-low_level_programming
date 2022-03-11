2a01164b6a0ddf0b6055a80774a351e61d01adc7
Minte123 <netacademy1986minte@yahoo.com>
2022-03-11 15:42:14 +0300

    The Number

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,13 +5,14 @@
 *Return: Always 0 (Success)
 */
 
-int main(void){
-int i = 48;
-while(i < 58)
+int main(void)
 {
-putchar(i);
+int i = 48;
+while (i < 58)
+{
+putchar (i);
 i++;
 }
-putchar(10);
-return(0);
+putchar (10);
+return (0);
 }
