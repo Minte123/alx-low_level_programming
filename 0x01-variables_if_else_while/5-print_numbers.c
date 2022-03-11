752d1a359c7478b99af93f603f6e851f324be54a
Minte123 <netacademy1986minte@yahoo.com>
2022-03-11 15:39:40 +0300

    The Number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+*main - print single digit numbers
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void){
+int i = 48;
+while(i < 58)
+{
+putchar(i);
+i++;
+}
+putchar(10);
+return(0);
+}
