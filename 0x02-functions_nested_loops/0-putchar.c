9286999d176ef5da66189150212bedefe65609bc
Minte123 <netacademy1986minte@yahoo.com>
2022-03-15 13:25:51 +0300

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "holberton.h"
 /**
  * main - Check the code
  *
@@ -7,15 +7,15 @@
 
 int main(void)
 {
-	char text[12] = "Alx School";
+	char text[10] = "Holberton";
 	int i = 0;
 
-	for(i=0; i < 11; i++)
+	for(i=0; i < 9; i++)
 	{
 		_putchar(text[i]);
 	}
 
-	_putchar(\n);
+	_putchar('\n');
 
 	return 0;
 }
