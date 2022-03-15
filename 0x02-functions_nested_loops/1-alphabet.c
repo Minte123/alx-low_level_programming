6cc13cb5fc98a12b5102c0e0e8afb1b80b36428d
Minte123 <netacademy1986minte@yahoo.com>
2022-03-15 16:07:33 +0300

    Lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/* main -Entry point
+ *
+ * Return 0 Always Successe
+ *
+ */
+main()
+{
+void print_alphabet(void);
+
+char c1;
+
+c1 = 97;
+
+for(c1 = 97; c1 <= 122; c1++)
+{
+	_putchar('%c \n');
+}
+return 0;
+}
+
