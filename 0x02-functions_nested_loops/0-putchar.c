c963f7308bd95f35f7f5b30013cba311fc89ce34
Minte123 <netacademy1986minte@yahoo.com>
2022-03-15 12:49:59 +0300

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,22 @@
-#include<stdio.h>
-main()
+#include<main.h>
+/**
+ * main - Check the code
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
 {
-	putchar("_putchar");
+	char text[12] = "Alx School";
+	int i = 0;
+
+	for(i=0; i < 11; i++)
+	{
+		_putchar(text[i]);
+	}
+
+	_putchar(\n);
+
 	return 0;
 }
 
