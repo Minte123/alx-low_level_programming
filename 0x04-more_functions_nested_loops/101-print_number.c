c8690b3a3f0b850032f98fa43d5bdd380e0ee1fa
Minte123 <netacademy1986minte@yahoo.com>
2022-03-17 16:15:17 +0300

    The integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,63 +1,11 @@
-#include <math.h>
 #include "main.h"
-
 /**
- * power - exponents
- * @base: base
- * @exp: exponent
- * Return: result (int)
+ * print_number - a function that prints an integer
+ * @n: input
+ * Return: inputted integer
  */
-
-int  power(int base, int exp)
-{
-	int i, num;
-
-	num = 1;
-	for (i = 0; i < exp; ++i)
-		num *= base;
-
-	return (num);
-}
-
-
 void print_number(int n)
 {
-	int negative = 0;
-	int digit;
-	int divisor;
-	int begin = 0;
-	int place = 10;
 
-	if (n < 0)
-	{
-		negative = 1;
-		n = n * -1;
-	}
-	while (place >= 0)
-	{
-	
-		divisor = power(10, place);
-		digit = ((n / divisor) % 10);
-		if (digit == 0 && begin == 0)
-		{
-			place--;
-		}
-		else if (digit != 0 && begin == 0)
-		{
-			begin = 1;
-			if (negative == 1)
-				_putchar('-');
-			_putchar('0' + digit);
-			place--;
-		}
-		else
-		{
-			_putchar('0' + digit);
-			place--;
-		}
-	}
-	if (digit == 0 && divisor == 1)
-	{
-		_putchar(48);
-	}
+	return (0);
 }
