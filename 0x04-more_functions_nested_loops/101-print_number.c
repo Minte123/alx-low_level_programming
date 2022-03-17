0ca9b92bf1b2d5b2adcc3e723414bf5f5bcd1ac6
Minte123 <netacademy1986minte@yahoo.com>
2022-03-17 14:33:08 +0300

    Integer Number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,50 +1,27 @@
 #include "main.h"
-
 /**
- * print_number - print number
- * @n: input value
- *
- * Description:  prints an integer
- * Return: Always (0)
+ * print_number - check numbers
+ * @n: input
+ * Return: success
  */
-
 void print_number(int n)
 {
-	if ((n < 0) && (n >= -9))
-	{
-		_putchar('-');
-		_putchar((n * -1) + '0');
-	}
-	else if (n <= -10)
-	{
-		_putchar('-');
-		_putchar(((n / 10) * -1) + '0');
-		_putchar(((n % 10)  * -1) + '0');
-	}
-	else if (n == 0)
-	{
-		_putchar(n + '0');
-	}
-	else if ((n > 0) && (n <= 9))
-	{
-		_putchar(n + '0');
-	}
-	else if ((n >= 10) && (n <= 99))
-	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else if ((n >= 100) && (n <= 999))
-	{
-		_putchar((n / 100) + '0');
-		_putchar(((n / 10) % 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else if ((n >= 1000) && (n <= 9999))
-	{
-		_putchar((n / 1000) + '0');
-		_putchar((n / 100) % 10 + '0');
-		_putchar((n / 10) % 10 + '0');
-		_putchar((n % 10) + '0');
-	}
+if (n < 0)
+{
+_putchar('-');
+n = -n;
+}
+if (n / 10 != 0 && n < 100)
+{
+_putchar('0' + n / 10);
+}
+else if (n / 10 != 0 && n < 1000)
+{
+_putchar(n / 100 + '0');
+}
+else if (n / 10 != 0 && n < 10000)
+{
+_putchar(n / 1000 + '0');
+}
+_putchar(n % 10 + '0');
 }
