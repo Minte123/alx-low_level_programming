e31ccb8064cfdcc3b9eea14814ce8b7fa37dc8dc
Minte123 <netacademy1986minte@yahoo.com>
2022-03-17 15:05:36 +0300

    Integer Number

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,27 +1,68 @@
+#include <math.h>
 #include "main.h"
+
 /**
- * print_number - check numbers
- * @n: input
- * Return: success
+ * power - exponents
+ * @base: base
+ * @exp: exponent
+ * Return: result (int)
  */
+
+int  power(int base, int exp)
+{
+	int i, num;
+
+	num = 1;
+	for (i = 0; i < exp; ++i)
+		num *= base;
+
+	return (num);
+}
+
+/**
+ * print_number - prints an integer
+ * @n: number to print
+ * Return void
+ */
+
 void print_number(int n)
 {
-if (n < 0)
-{
-_putchar('-');
-n = -n;
-}
-if (n / 10 != 0 && n < 100)
-{
-_putchar('0' + n / 10);
-}
-else if (n / 10 != 0 && n < 1000)
-{
-_putchar(n / 100 + '0');
-}
-else if (n / 10 != 0 && n < 10000)
-{
-_putchar(n / 1000 + '0');
-}
-_putchar(n % 10 + '0');
+	int negative = 0;
+	int digit;
+	int divisor;
+	int begin = 0;
+	int place = 10;
+
+	if (n < 0)
+	{
+		negative = 1;
+		n = n * -1;
+	}
+	while (place >= 0)
+	{
+		/*divisor = pow(10, place);*/
+		divisor = power(10, place);
+		digit = ((n / divisor) % 10);
+		if (digit == 0 && begin == 0)
+		{
+			place--;
+		}
+		else if (digit != 0 && begin == 0)
+		{
+			begin = 1;
+			if (negative == 1)
+				_putchar('-');
+			_putchar('0' + digit);
+			place--;
+		}
+		else
+		{
+			_putchar('0' + digit);
+			place--;
+		}
+	}
+	if (digit == 0 && divisor == 1)
+	{
+		_putchar(48);
+	}
 }
