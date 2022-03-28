d947942482719322b6060f0702a41af7b29e4513
Minte123 <netacademy1986minte@yahoo.com>
2022-03-28 15:48:32 +0300

    Function Substring

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,38 +1,34 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * _strstr - locates a substring
- * @haystack: string to search through
- * @needle: string to match
- * Return: pointer to initial segment of haystack
+ * _strstr - locate a substring
+ * @haystack: string
+ * @needle: string substring
+ * Return: Pointer to beginning of substring, or NULL if not found.
  */
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j, start;
-	int nlen = 0;
+	int i, j, c;
 
 	i = 0;
-	j = 0;
-	start = 0;
-
-	while (needle[nlen] != '\0')
+	c = 0;
+	while (haystack[i] != '\0')
 	{
-		nlen++;
-	}
-	for (i = 0; haystack[i] != '\0'; i++)
-	{
-		for (j = 0; j < nlen && haystack[i] == needle[j]; j++, i++)
+		j = 0;
+		while (needle[j + c] != '\0' && haystack[i + c] != '\0'
+		       && needle[j + c] == haystack[i + c])
 		{
-			if (j == 0)
-			{
-				start = i;
-			}
-			if (j == nlen - 1)
-			{
-				return (haystack + start);
-			}
+			if (haystack[i + c] != needle[j + c])
+				break;
+			c++;
 		}
+		if (needle[j + c] == '\0')
+			return (&haystack[i]);
+		j++;
+		i++;
 	}
-	return (0);
+
+	return (NULL);
 }
