9360c1fef15f0b8603462f48f6939e6fe706ad9d
Minte123 <netacademy1986minte@yahoo.com>
2022-03-30 15:21:03 +0300

    Root of number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,33 +1,40 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - return the natural square root of a number
- * @n: int number
- * Return: If no natural square root, return -1. Else return natural
- * square root
+ * check_int - checks integer as candidate for square root
+ * @square: square integer
+ * @x: integer
+ * Return: 1 if
+ */
+
+int check_int(int square, int x)
+{
+	if (x * x == square)
+		return (x);
+	else if (x > square / x)
+		return (check_int(square, x - 1));
+	else if (x < square / x)
+		return (check_int(square, x + 1));
+	else
+		return (-1);
+}
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: integer
+ * Return: integer - square root, if none returns -1
  */
 
 int _sqrt_recursion(int n)
 {
-	return (halp(n, 1));
-}
+	int start;
 
-/**
- * halp - helper function to solve _sqrt_recursion
- * @c: number to determine if square root
- * @i: incrementer to compare against `c`
- * Return: square root if natural square root, or -1 if none found
- */
+	start = 1;
 
-int halp(int c, int i)
-{
-	int square;
-
-	square = i * i;
-	if (square == c)
-		return (i);
-	else if (square < c)
-		return (halp(c, i + 1));
-	else
+	if (n < 0)
 		return (-1);
+	else if (n == 1)
+		return (1);
+	else
+		return (check_int(n, start));
 }
