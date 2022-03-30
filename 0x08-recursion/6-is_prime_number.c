4644b39e6c12e064f2cb0c2c4dab98ec13cf7629
Minte123 <netacademy1986minte@yahoo.com>
2022-03-30 15:29:17 +0300

    Return Function

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,51 +1,39 @@
 #include "main.h"
-
 /**
- * is_prime_number - determine if a number is a prime number
- * @n: int number
- * Return: 1 if prime, 0 otherwise
- */
-
+ * sqtRecursive - computes square root recursively
+ * @n: given number
+ * @m: comparison number
+ * Return: 1 if not found sqrroot, else sqrroot
+ **/
+int sqtRecursive(int n, int m)
+{
+	if (n <= 0)
+		return (-1);
+	if (n * n == m)
+		return (n);
+	return (sqtRecursive(n - 1, m));
+}
+/**
+ * _sqrt_recursion - finds the natural square root of a number
+ * @n: given number
+ * Return: square root of n or -1
+ **/
+int _sqrt_recursion(int n)
+{
+	if (n == 1)
+		return (1);
+	return (sqtRecursive(n / 2, n));
+}
+/**
+ * is_prime_number - checks if a given number is prime
+ * @n: given number
+ * Return: 1 if number is prime else 0
+ **/
 int is_prime_number(int n)
 {
-	if (n < 2)
+	if (n <= 1 || _sqrt_recursion(n) >= 1)
 		return (0);
-	if (n < 4)
+	if (_sqrt_recursion(n) == -1)
 		return (1);
-	return (hai(n, 2));
-}
-
-/**
- * _sqrt - return square root of number
- * @x: number
- * @i: number incrementer acting as divisor
- * Return: square root of `x`
- */
-
-int _sqrt(int x, int i)
-{
-	int square;
-
-	square = i * i;
-	if (square >= x)
-		return (i);
-	else
-		return (_sqrt(x, i + 1));
-}
-
-/**
- * hai - helper function, recursive steps taken
- * @n: number given to original function is_prime_number
- * @d: incrementer divisor
- * Return: 0 if not prime, 1 if prime
- */
-
-int hai(int n, int d)
-{
-	if (n % d == 0)
-		return (0);
-	else if (_sqrt(n, 1) < d)
-		return (1);
-	else
-		return (hai(n, d + 1));
+	return (_sqrt_recursion(n));
 }
