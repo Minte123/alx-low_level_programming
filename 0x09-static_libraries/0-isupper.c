b2a7b72173a0fcc30ffe7475e00a17d94c5066a4
Minte123 <netacademy1986minte@yahoo.com>
2022-04-01 19:01:22 +0300

    The Stat

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,15 +1,26 @@
 #include "main.h"
 
 /**
- * _isupper - checks for uppercase
- * @c: character to checked for case
+ * _strncpy - Copies at most an inputted number
+ *            of bytes from string src into dest.
+ * @dest: The buffer storing the string copy.
+ * @src: The source string.
+ * @n: The maximum number of bytes to copied from src.
  *
- * Return: 1 if uppercase, 0 if not uppercase
+ * Return: A pointer to the resulting string dest.
  */
-
-int _isupper(int c)
+char *_strncpy(char *dest, char *src, int n)
 {
-	if (c >= 'A' && c <= 'Z')
-		return (1);
-	return (0);
+	int index = 0, src_len = 0;
+
+	while (src[index++])
+		src_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[index] = src[index];
+
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
+
+	return (dest);
 }
