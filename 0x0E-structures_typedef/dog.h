a757130c5d5f385cb08516e37d835991b462b371
Minte123 <netacademy1986minte@yahoo.com>
2022-04-11 13:05:47 +0300

    The Task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,20 +1,21 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE dog.h
+#ifndef DOG_H
+#define DOG_H
+
 /**
- * struct dog - Define a new struct dog with name, age, owner
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of dog
- **/
-struct dog
+ * struct dog - structure definition of a dog
+ * @name: character string
+ * @age: integer
+ * @owner: character string
+ */
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 #endif
